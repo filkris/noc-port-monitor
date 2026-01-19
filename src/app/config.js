85b1c01fbbe8f75c6069c98f80c84efe5fbcf436
filkris <36194724+filkris@users.noreply.github.com>
2026-01-19 10:06:54 +0100

    Fetch router logs

---
+++ src/app/config.js
@@ -0,0 +1,8 @@
+export const UID = 370;
+
+export const LOCALE = Object.freeze({
+	LANG: "sr@latin",
+	TZ: "Europe/Belgrade",
+});
+
+export const COMPANY_IDS = Object.freeze([1]);
---
+++ src/app/constants/api.js
@@ -0,0 +1,7 @@
+export const API_BASE = "https://nocportal.telekom.rs";
+
+export const API_ENDPOINTS = Object.freeze({
+	CALL_BUTTON: "/web/dataset/call_button",
+	READ_WIZARD: "/web/dataset/call_kw/mts.router.switch.command.wizard/read",
+	CREATE_WIZARD: "/web/dataset/call_kw/mts.router.switch.command.wizard/create",
+});
