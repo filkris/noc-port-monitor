54008a4fe2784111fd5f030acad2f948a67dc8ad
filkris <36194724+filkris@users.noreply.github.com>
2026-01-19 20:27:11 +0100

    Add disabled

--- src/app/components/Select.jsx
+++ src/app/components/Select.jsx
@@ -1,9 +1,10 @@
-export default function Select({ value, onChange, options, className = "" }) {
+export default function Select({ value, onChange, options, className = "", disabled = false }) {
 	return (
 		<select
 			value={value}
 			onChange={(e) => onChange(e.target.value)}
 			className={className}
+			disabled={disabled}
 		>
 			{options.map((option) => (
 				<option key={option.value} value={option.value}>
