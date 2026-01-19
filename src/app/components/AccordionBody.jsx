b4ba096d4f47d76cdfe08f2ba4af6140097f02d5
filkris <36194724+filkris@users.noreply.github.com>
2026-01-19 19:57:36 +0100

    Fix padding

--- src/app/components/AccordionBody.jsx
+++ src/app/components/AccordionBody.jsx
@@ -4,15 +4,11 @@ export default function AccordionBody({ ports = {} }) {
 	const portEntries = Object.entries(ports);
 
 	if (portEntries.length === 0) {
-		return (
-			<div className="px-3 pb-3 text-xs text-gray-400">
-				No data yet...
-			</div>
-		);
+		return <div className="p-3 text-xs text-center text-gray-400">Nothing to show here...</div>;
 	}
 
 	return (
-		<div className="px-3 pb-3">
+		<div className="p-3">
 			{portEntries.map(([portId, events]) => (
 				<Port key={portId} portId={portId} events={events} />
 			))}
