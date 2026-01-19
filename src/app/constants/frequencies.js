e16d5efae041fa85db4faaf51f7ad7da5fc5628c
filkris <36194724+filkris@users.noreply.github.com>
2026-01-19 09:49:23 +0100

    Add constants

---
+++ src/app/constants/frequencies.js
@@ -0,0 +1,6 @@
+export const FREQUENCIES = Object.freeze([
+	{ value: "1h", label: "Every 1 hour" },
+	{ value: "2h", label: "Every 2 hours" },
+	{ value: "3h", label: "Every 3 hours" },
+	{ value: "6h", label: "Every 6 hours" },
+]);
