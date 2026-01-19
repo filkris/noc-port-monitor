c5946a8d445fe29b48dad9bb042cb597cf804621
filkris <36194724+filkris@users.noreply.github.com>
2026-01-19 09:55:31 +0100

    Remove border

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,7 +3,7 @@ import Manual from "./Manual";
 
 export default function Header() {
 	return (
-		<header className="border-t-2 border-primary bg-white">
+		<header>
 			<Scheduler />
 			<Manual />
 		</header>
